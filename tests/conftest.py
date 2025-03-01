0734ccb4ad7e691e5d5f87732a0b4aaa0ff2f94a
Lennart Buhl <git@hackmate.de>
2025-03-01 14:31:06 +0100

    Use new agents repo

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,5 @@ import pytest
 
 @pytest.fixture
 def wasm_agent():
-    with open(pathlib.Path(__file__).parents[1] / "examples" / "agent.wasm", "rb") as f:
+    with open(pathlib.Path(__file__).parents[1] / "agents" / "example" / "agent.wasm", "rb") as f:
         return f.read()
